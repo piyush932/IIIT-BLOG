12dc796d08c4dc37ef35cf4c2dd5c118bd2df25c
Piyush <b521044@iiit-bh.ac.in>
2024-02-11 12:02:30 +0530

    add signout functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { updateUser,test,deleteUser } from '../controllers/user.contoller.js'
+import { updateUser,test,deleteUser,signout } from '../controllers/user.contoller.js'
 
 const router = express.Router()
 
 router.get('/test',test)
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser)
+router.post('/signout',signout)
 
 export default router
