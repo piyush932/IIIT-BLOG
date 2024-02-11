117841693401686fa3d0a4444413592afd9e270f
Piyush <b521044@iiit-bh.ac.in>
2024-02-11 09:52:56 +0530

    update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { test } from '../controllers/user.contoller.js'
+import { verifyToken } from '../utils/verifyUser.js'
+import { updateUser } from '../controllers/user.contoller.js'
 
 const router = express.Router()
 
 router.get('/test',test)
+router.put('/update/:userId',verifyToken,updateUser);
 
 export default router
