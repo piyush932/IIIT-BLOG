1613f6b1e11b14468e1257cf317e6022cdccc595
Piyush <b521044@iiit-bh.ac.in>
2024-03-23 10:08:14 +0530

    add like functionality to the comment component

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { updateUser,test,deleteUser,signout,getUsers } from '../controllers/user.contoller.js'
+import { updateUser,test,deleteUser,signout,getUsers,getUser } from '../controllers/user.contoller.js'
 
 const router = express.Router()
 
@@ -9,6 +9,6 @@ router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser)
 router.post('/signout',signout)
 router.get('/getusers', verifyToken, getUsers);
-
+router.get('/:userId', getUser);
 
 export default router
